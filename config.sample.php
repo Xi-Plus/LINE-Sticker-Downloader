c38976f7e6f489087b354dc1ad7acda1afee1086
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-04-08 20:27:54 +0800

    add config.sample.php

---
+++ config.sample.php
@@ -0,0 +1,7 @@
+<?php
+$config["type"]=1; // 1 = 從 sticker shop 下載； 2 = 從圖片連結下載
+$config["sticker_id"]=1256753; // Ex: https://store.line.me/stickershop/product/1183925/zh-Hant
+// 如果 type = 2 以下才需要填寫
+$config["photo_id"]=7480388; // Ex: https://sdl-stickershop.line.naver.jp/products/0/0/1/1183925/android/stickers/7480388.png
+$config["photo_count"]=40;
+?>
